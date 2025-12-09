e6faf7bc5f5478b42f3ec0381114693fe348201a
shalakakashikar-sudo <shalakakashikar@gmail.com>
2025-12-09 22:29:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// IMPORTANT:
+// The "base" must match your repository name exactly.
+// Since your repo is SVA-Guide, the base is "/SVA-Guide/"
+
+export default defineConfig({
+  base: "/SVA-Guide/",
 });
